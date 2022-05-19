16a8217f2a682635c700278b5729718b46bbd9c2
FernandoSilvera <fernando.silvera02@gmail.com>
2022-05-19 16:36:10 +0200

    Fix.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let inputName = document.getElementById("name");
+let userKm = document.querySelector("[name='userKm']");
+let userAge = document.querySelector("[name='userAge']");
+const submit = document.getElementById("submitBtn");
+const erase = document.getElementById("eraseBtn");
+
+
+
+submit.addEventListener("click", createTicket());
+
+function createTicket() {
+    console.log(inputName.value, userKm.value, userAge.value);
+}
