ff50435cd15618cc511ac4366ed622f1d36a6837
FernandoSilvera <fernando.silvera02@gmail.com>
2022-05-19 17:16:47 +0200

    Making ticket section.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-let inputName = document.getElementById("name");
+let userName = document.querySelector("[name='userName']");
 let userKm = document.querySelector("[name='userKm']");
 let userAge = document.querySelector("[name='userAge']");
+let spanName = document.getElementById("span-name");
+let spanCarriage = document.getElementById("span-carriage");
+let spanOffer = document.getElementById("span-offer");
+let spanPrice = document.getElementById("span-price");
 const submit = document.getElementById("submitBtn");
 const erase = document.getElementById("eraseBtn");
 
 
+erase.addEventListener("click", function createTicket(){
+    userName.value = "";
+    userKm.value = "";
+});
+
 submit.addEventListener("click", function createTicket(){
-    console.log(inputName.value, userKm.value, userAge.value);
+    spanName.innerHTML = userName.value;
+
+    if (userAge.value === "under"){
+        spanOffer.innerHTML = "20% Discount";
+    } else if (userAge.value === "over"){
+        spanOffer.innerHTML = "40% Discount";
+    } else{
+        spanOffer = "No discount";
+    }
+
+    userName.value = "";
+    userKm.value = "";
 });
 
